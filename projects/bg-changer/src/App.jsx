8013d7cd8cd31d9410f1685c4584d324abf3b487
marlingeshwar26brillio <maralingeshwar.m@brillio.com>
2026-04-02 15:52:58 +0530

    projects doings

---
+++ projects/bg-changer/src/App.jsx
@@ -0,0 +1,22 @@
+function App() {
+
+  function dis() {
+    console.log("Faaaaaaaa")
+  }
+
+  return (
+    <>
+      <div className="main-container">
+        <h1>Hello soni kuddi hai yaraaa</h1>
+        <div className="btns">
+          <button onClick={dis}>Red</button>
+          <button>Yellow</button>
+          <button>Green</button>
+          <button>Purple</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
